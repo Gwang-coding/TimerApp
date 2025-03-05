6ad88baec3272f99027a9f1282f07f8c08ec202d
gwang-coding <jaegwang1238@gmail.com>
2025-03-06 01:49:01 +0900

    feat: add audio

--- src/components/TrackData.ts
+++ src/components/TrackData.ts
@@ -5,19 +5,23 @@ export interface Track {
 }
 
 export const categoryTracks: Record<string, Track[]> = {
-    카페: [
-        { id: 1, title: '카페 분위기 1', src: '../assets/audio/pocketmon1.mp3' },
-        { id: 2, title: '카페 분위기 2', src: '../assets/audio/pocketmon2.mp3' },
-        { id: 3, title: '카페 분위기 3', src: '../assets/audio/pocketmon3.mp3' },
+    jazz: [
+        { id: 1, title: 'latte', src: '../assets/audio/jazz/latte.mp3' },
+        { id: 2, title: 'midnight', src: '../assets/audio/jazz/midnight.mp3' },
+        { id: 3, title: 'sebuah', src: '../assets/audio/jazz/sebuah.mp3' },
+        { id: 4, title: 'smooth', src: '../assets/audio/jazz/smooth.mp3' },
+        { id: 5, title: 'smoothcafe', src: '../assets/audio/jazz/smoothcafe.mp3' },
+        { id: 6, title: 'smoothchill', src: '../assets/audio/jazz/smoothchill.mp3' },
     ],
-    자연: [
-        { id: 1, title: '자연 사운드 1', src: '../assets/audio/pocketmon2.mp3' },
-        { id: 2, title: '자연 사운드 2', src: '../assets/audio/pocketmon3.mp3' },
-        { id: 3, title: '자연 사운드 3', src: '../assets/audio/pocketmon1.mp3' },
+    lofi: [
+        { id: 1, title: 'blissfulmorning', src: '../assets/audio/lofi/blissfulmorning.mp3' },
+        { id: 2, title: 'bubbletea', src: '../assets/audio/lofi/bubbletea.mp3' },
+        { id: 3, title: 'cosydreaming', src: '../assets/audio/lofi/cosydreaming.mp3' },
+        { id: 4, title: 'feelingood', src: '../assets/audio/lofi/feelingood.mp3' },
+        { id: 5, title: 'perfectday', src: '../assets/audio/lofi/perfectday.mp3' },
     ],
-    일렉: [
-        { id: 1, title: '일렉 트랙 1', src: '../assets/audio/pocketmon4.mp3' },
-        { id: 2, title: '일렉 트랙 2', src: '../assets/audio/pocketmon3.mp3' },
-        { id: 3, title: '일렉 트랙 3', src: '../assets/audio/pocketmon2.mp3' },
+    nature: [
+        { id: 1, title: 'firecamp', src: '../assets/audio/nature/firecamp.mp3' },
+        { id: 2, title: 'raining', src: '../assets/audio/nature/raining.mp3' },
     ],
 };
