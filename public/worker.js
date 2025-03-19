6a914da425b99f90f52a602f6095e4eba22964a5
gwang-coding <jaegwang1238@gmail.com>
2025-03-19 23:46:37 +0900

    Feat: worker.js to use timer on background

---
+++ public/worker.js
@@ -0,0 +1,23 @@
+let time = 0;
+let intervalId = null;
+console.log('hi');
+self.onmessage = function (e) {
+    if (e.data === 'start') {
+        if (!intervalId) {
+            intervalId = setInterval(() => {
+                time += 1;
+                self.postMessage(time);
+            }, 1000);
+        }
+    } else if (e.data === 'stop') {
+        clearInterval(intervalId);
+        intervalId = null;
+    } else if (e.data === 'reset') {
+        time = 0;
+        self.postMessage(time);
+    } else if (typeof e.data === 'object' && e.data.command === 'set') {
+        // 새로운 시간 값을 설정하는 메시지 처리
+        time = e.data.time;
+        self.postMessage(time);
+    }
+};
